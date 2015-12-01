65e198040cc03aaba4320807ecbff6e35d0636b8
邹军华 <zoujunhua@meizu.com>
2015-12-01 20:16:42 +0800

    Add lg vendor camera in the FlymeLauncher folder
    Change-Id: Ia00608e4bd0e2f4cb178071f8c50bb677e85fb17

--- custom_app.sh
+++ custom_app.sh
@@ -6,6 +6,10 @@ tempSmaliDir=$2
 
 if [ "$apkBaseName" = "TelephonyProvider" ];then
         echo ">>> in custom_app for $apkBaseName to adjust the interface "getAllMessagesFromIcc" with "getAllMessagesFromIccExtended""
-        find $tempSmaliDir/ -name "*.smali" | xargs sed -i 's#invoke-static {}, Landroid\/telephony\/SmsManager;->getAllMessagesFromIcc()Ljava\/util\/ArrayList#invoke-static {}, Landroid\/telephony\/SmsManager;->getAllMessagesFromIccExtended()Ljava\/util\/ArrayList#g' 
+        find $tempSmaliDir/ -name "*.smali" | xargs sed -i 's#invoke-static {}, Landroid\/telephony\/SmsManager;->getAllMessagesFromIcc()Ljava\/util\/ArrayList#invoke-static {}, Landroid\/telephony\/SmsManager;->getAllMessagesFromIccExtended()Ljava\/util\/ArrayList#g'
+
+elif [ "$apkBaseName" = "FlymeLauncher" ];then
+        echo ">>> in custom_app for $apkBaseName to add lg vendor camera in the folder"
+        sed -i '/<favorite launcher:className="com.meizu.feedback.activity.MainActivity" launcher:packageName="com.meizu.feedback" \/>/a\<favorite launcher:className="com.lge.camera.CameraAppLauncher" launcher:packageName="com.lge.camera" \/>' $tempSmaliDir/res/xml/default_workspace.xml
 
 fi
